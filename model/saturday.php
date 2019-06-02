6adf6127422570e46adeec83bdc05365202bab7d
mgallardo3 <mgallardo3@mail.greenriver.edu>
2019-06-02 00:03:36 -0400

    Add a Saturday class that inherits methods from Events class

---
+++ model/saturday.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * 06/01/2019
+ * This file contains the class event Saturday that inherits functionality from the class events
+ *
+ * @authors Maria Gallardo <mgallardo3@mail.greenriver.edu>,Kat Truitt <ktruitt@mail.greenriver.edu>
+ * @copyright 2019
+ */
+class Saturday extends Events
+{
+    //inherits methods from Events
+}
